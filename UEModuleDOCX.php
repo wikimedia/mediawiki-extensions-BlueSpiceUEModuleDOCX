80658e87d4f931446aec6e7ee8db867e2c40675a
rvogel <vogel@hallowelt.biz>
2019-09-19 09:03:15 +0200

    Fix icon
    ERM13370
    
    [3.1]
    
    NEEDS CHERRY-PICK TO `master`!
    
    Change-Id: I3e12667e48114ce4c871a5bf407fb5a8784826d8

--- UEModuleDOCX.php
+++ UEModuleDOCX.php
@@ -127,7 +127,7 @@ class UEModuleDOCX extends BsExtensionMW {
 			'title' => wfMessage( 'bs-uemoduledocx-widgetlink-single-title' )->plain(),
 			'text' => wfMessage( 'bs-uemoduledocx-widgetlink-single-text' )->plain(),
 			'class' => 'bs-ue-export-link',
-			'iconClass' => 'icon-file-word icon-image'
+			'iconClass' => 'icon-file-word'
 		];
 
 		$template->data['bs_export_menu'][] = $contentActions;
